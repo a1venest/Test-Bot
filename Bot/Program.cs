a98e12b8e884e79236efbba5d5ee0b70ae7f4713
Aivenest <aivenest@gmail.com>
2023-02-04 13:09:07 +0500

    1.0.0

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Telegram.Bot;
+
+string token = "TOKEN";
+var client = new TelegramBotClient(token);
+
+string GetAnswer(string msg)
+{
+    string answer = "🤝";
+    if (msg.Contains("hi"))
+    {
+        answer = "Дратути!";
+    }
+    return answer;
+}
+
+client.StartReceiving(
+(c, arg, _) =>
+{
+    return c.SendTextMessageAsync(arg.Message.Chat.Id,
+    GetAnswer(arg.Message.Text.ToLower()));
+},
+(_, _, _) => Task.CompletedTask
+
+);
+Console.WriteLine("StartReceiving ...");
+Console.ReadLine();
